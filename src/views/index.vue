0fa6d1456c93f1d3151a0c27058240353699d427
neokoni <dulin081682@126.com>
2024-10-01 21:57:25 +0800

    init done: 主体页面

---
+++ src/views/index.vue
@@ -0,0 +1,137 @@
+<template>
+    <!-- 主体方框部分 -->
+    <div class="
+        h-[400px] w-[900px] bg-[rgba(143,143,143,0.5)] backdrop-blur-xl 
+        border-[rgba(181,181,181,1)] border rounded-[60px]
+        m-auto absolute top-0 left-0 right-0 bottom-0 bg-no-repeat
+        pt-[50px] pl-[50px] pr-[50px] pb-[50px]
+        flex items-center justify-between
+        text-[rgba(227,227,227,1)] text-base
+        shadow-2xl">
+        <div class=" max-w-[350px] h-full">
+            <p class="text-3xl ">捐赠(打钱)</p>
+            <p class=" pt-3">感谢你的捐赠或向我付款，请选择你喜欢的支付渠道以展开支付，点击以展开详情</p>
+        </div>
+        <!-- 中间分隔线 -->
+        <div class="ml-6 h-64 block border border-[rgba(153,151,151,1)] bg-[rgba(153,151,151,1)]"></div>
+        <div class="ml-6 w-[300px] h-full text-center">请选择支付方式
+            <!-- 使用v-for循环进行生成，在const payWay那里 -->
+            <div v-for="i in payWay">
+                <button class="border h-16 w-48 mt-4 rounded-[67px] border-[#C7BBBB] bg-[rgba(191,187,187,0.3)]
+                     hover:bg-[rgba(197,197,197,0.71)] hover:text-white hover:shadow-xl
+                      transition ease-in-out duration-500" @click="openQRcode(i.type)">
+                    <img :src="i.logo" alt="">
+                    <p v-text="i.name"></p>
+                </button>
+            </div>
+        </div>
+    </div>
+    <!-- 覆盖部分 -->
+    <div class="absolute left-0 top-0 bg-[rgb(143,143,143,0.5)] 
+                h-screen w-screen transition ease-in-out duration-500
+                text-base text-[rgba(196,196,196,1)]
+                backdrop-blur-2xl hidden" id="qrcodepage">
+        <div class="">
+            <p class=" text-center pt-60">请使用<span id="payTypeTitle"></span>扫描二维码</p>
+            <!-- 二维码主体部分 -->
+            <div class="h-[280px] w-[280px] bg-[rgba(107,107,107,0.3)]
+                        border border-[rgba(145,145,145,1)] rounded-2xl
+                        m-auto mt-10
+                        pt-[10px] pl-[10px] pr-[10px] pb-[10px]">
+                <img src="" alt="" id="qrcode" class="block">
+            </div>
+            <!-- 跳转按钮部分 -->
+            <a id="openApp" class="hidden" target="_blank">
+                <button class=" m-auto w-[250px] h-[70px] bg-[rgba(107,107,107,0.2)]
+                                    border border-[rgba(145,145,145,1)] rounded-full
+                                    mt-10 text-center block 
+                                    hover:bg-[rgba(197,197,197,0.71)] hover:text-white hover:shadow-xl
+                                    transition ease-in-out duration-500 
+                                    ">或直接跳转支付宝?</button>
+            </a>
+        </div>
+        <!-- 关闭按钮处 -->
+        <div class="w-10 h-10 bg-[rgba(107,107,107,0.2)] absolute 
+                        top-40 right-48 hover:shadow-xl
+                        border border-[rgba(145,145,145,1)] rounded-full
+                        hover:bg-[rgba(59,59,59,0.2)] transition ease-in-out duration-500" @click="closeQRcode()">
+            <img src="../assets/icons/x.svg" alt="" class="m-auto pt-2 fill-white">
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data() {
+        return {
+            // 支付方式
+            payWay: [
+                {
+                    name: "支付宝",
+                    logo: "",
+                    type: "alipay"
+                },
+                {
+                    name: "微信",
+                    logo: "",
+                    type: "wechat"
+                },
+                {
+                    name: "QQ钱包",
+                    logo: "",
+                    type: "qq"
+                }
+            ]
+        };
+    },
+    // @click用到的方法
+    methods: {
+        closeQRcode() {
+            // 获取元素
+            const qrcodePage = document.getElementById("qrcodepage");
+            const qrcode = document.getElementById("qrcode");
+            const payTypeTitle = document.getElementById("payTypeTitle")
+            // 还原到初始值
+            qrcode?.setAttribute("src", "");
+            payTypeTitle.textContent = "";
+            qrcodePage?.classList.add("hidden");
+            document.getElementById("openApp").classList.add("hidden");
+        },
+        openQRcode(payPlatform: string) {
+            // 获取元素
+            const qrcodePage = document.getElementById("qrcodepage");
+            const qrcode = document.getElementById("qrcode");
+            const payTypeTitle = document.getElementById("payTypeTitle")
+            const openApp = document.getElementById("openApp");
+            // 判定选择的方式
+            if (payPlatform == "alipay") {
+                // 设置支付宝收款码
+                qrcode?.setAttribute("src", "src/assets/qrcodes/alipay.png");
+                // 设置显示的支付方式名称
+                payTypeTitle.textContent = "支付宝";
+                // 对其显示跳转按钮
+                document.getElementById("openApp").classList.remove("hidden");
+                // 跳转的链接
+                openApp?.setAttribute("href", "https://qr.alipay.com/fkx14150lufylbxvwp40u0a");
+            }
+            if (payPlatform == "wechat") {
+                // 设置微信收款码
+                qrcode?.setAttribute("src", "src/assets/qrcodes/wechat.png");
+                // 设置显示的支付方式名称
+                payTypeTitle.textContent = "微信";
+            }
+            if (payPlatform == "qq") {
+                // 设置QQ收款码
+                qrcode?.setAttribute("src", "src/assets/qrcodes/qq.png");
+                // 设置显示的支付方式名称
+                payTypeTitle.textContent = "QQ";
+            }
+            // 让显示
+            qrcodePage?.classList.remove("hidden");
+        },
+
+    }
+});
+</script>
