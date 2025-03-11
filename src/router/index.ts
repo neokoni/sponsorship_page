750a73b8f278ac09c9d6196c9c7e066301d36588
neokoni <dulin081682@126.com>
2025-03-11 16:05:19 +0800

    rerererefix: static page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     // {
     //   path: '/',
@@ -9,7 +9,7 @@ const router = createRouter({
     //   component: HomeView
     // },
      {
-       path: './',
+       path: '/',
        name: 'index',
        // route level code-splitting
        // this generates a separate chunk (About.[hash].js) for this route
